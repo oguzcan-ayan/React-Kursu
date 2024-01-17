bfe424249118ad2c5e1c4a5c27d43c9fe49691db
Oğuzcan Ayan <57589266+oguzcan-ayan@users.noreply.github.com>
2024-01-17 17:12:59 +0300

    advanced-level-1

---
+++ Ileri Seviyeler_1/deployment/surge.sh-deployment/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>Learn React</h2>
+      <h3>React Lifecycle</h3>
+    </div>
+  );
+}
+
+export default App;
