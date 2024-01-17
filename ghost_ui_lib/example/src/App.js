cf70f295015447ac77c3a98cbdadd27797078e8e
Oğuzcan Ayan <57589266+oguzcan-ayan@users.noreply.github.com>
2024-01-17 17:18:19 +0300

    my-ui-library

---
+++ ghost_ui_lib/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'ghost_ui_lib'
+import 'ghost_ui_lib/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
